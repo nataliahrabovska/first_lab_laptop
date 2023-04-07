3a4656494d4c7ab7155ac579a149352f987d3ea8
nataliahrabovska <natagrabovskanata@gmail.com>
2023-04-07 19:03:56 +0300

    Laptop

---
+++ Laptop/src/main/java/ua/lviv/iot/algo/part1/laptop3/MacBook.java
@@ -0,0 +1,28 @@
+package ua.lviv.iot.algo.part1.laptop3;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString(callSuper = true)
+
+public class MacBook extends AbstractLaptop {
+    private String color;
+    private int guaranteeInMonth;
+
+    public MacBook(String model, double screenSize, int ram, int storage, int batteryLife, int batteryLevel, String color, int guarantee) {
+        super(model, screenSize, ram, storage, batteryLife, batteryLevel);
+        this.color = color;
+        this.guaranteeInMonth = guarantee;
+    }
+
+    @Override
+    public void replaceBattery(int capacityInHours) {
+        this.batteryLife = capacityInHours;
+    }
+
+}
